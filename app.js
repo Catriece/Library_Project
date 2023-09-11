b4ba1f0b60f88dcb8b684b6f08a303ea2d7af494
Catriece Gilbert <119451056+Catriece@users.noreply.github.com>
2023-09-11 15:05:00 -0400

    Fixed first remove button. Resubmission

--- app.js
+++ app.js
@@ -1,100 +1,116 @@
-/* PRACTICE
-
 console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
 
 const books = [
-    {
-        id: 1,
-        title: "Name of the Wind",
-        author: "Patrick Rothfuss",
-        read: true,
-    },
+  {
+    id: 1,
+    title: "Name of the Wind",
+    author: "Patrick Rothfuss",
+    read: true,
+  },
 ];
 
 class Book {
-    constructor(id, title, author, read) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.read = read; 
-    }  
+  constructor(id, title, author, read) {
+    (this.id = id),
+      (this.title = title),
+      (this.author = author),
+      (this.read = read);
+  }
 }
 
-
 class Library {
-    constructor(books){
-     this.bookCount = books.length;
-     this.books = books;  
-    }
-    
-    addBook() {
-        const title = document.getElementById("title");
-        const author = document.getElementById("author");
-        const read = document.getElementById("read");
-        
-        const newBook = new Book(this.nextId, title.value, author.value, read.checked);
-        this.books.push(newBook);
-        
-        const tbody = document.getElementById("tableBody");
-        const newTr = document.createElement("tr");
-        newTr.classList.add(newbook.id);
-        const newTitle = document.createElement("td");
-        const newAuthor = document.createElement("td");
-        const newRead = document.createElement("td");
-        const newRemoveBtn = document.createElement("td")
+  constructor(books) {
+    (this.book_count = books.length), (this.books = books);
+  }
 
-        newTitle.textContent = title.value;
-        newAuthor.textContent = author.value;
-        const newCheckbox = document.createElement("input");
-        newCheckbox.classList.add(newBook.id);
-        newCheckbox.type = "checkbox";
-        newCheckbox.checked = read.checked;
-        newCheckbox.disabled = read.checked;
-        newCheckbox.addEventListener("click", (event) => {
-            this.markRead(event.target, parseInt(event.target.classList[0]));
-        });
+  addBook() {
+    const bookTitle = document.getElementById("title");
+    const bookAuthor = document.getElementById("author");
+    const bookRead = document.getElementById("read");
+    this.book_count++;
+    const createdBook = new Book(
+      this.book_count,
+      bookTitle.value,
+      bookAuthor.value,
+      bookRead.value
+    );
+    this.books.push(createdBook);
 
-        newRead.appendChild(newCheckbox);
-        newTr.appendChild(newTitle);
-        newTr.appendChild(newAuthor);
-        newTr.appendChild(newRead);
+    // adding book to table
+    const table = document.getElementById("tableBody");
+    const newTr = document.createElement("tr");
+    console.log(createdBook.id);
+    newTr.classList.add(createdBook.id);
+    newTr.id = "bookRow" + createdBook.id;
+    const titleData = document.createElement("td");
+    const authorData = document.createElement("td");
+    const readData = document.createElement("td");
+    const removeCell = document.createElement("td");
+    const removeButton = document.createElement("button");
+    removeButton.id = "remove";
+    removeButton.classList.add(createdBook.id);
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", (event) => {
+      this.removeBook(parseInt(event.target.classList[0]));
+    });
 
-        tbody.appendChild(newTr);
+    titleData.textContent = bookTitle.value;
+    authorData.textContent = bookAuthor.value;
+    const newCheckbox = document.createElement("input");
+    newCheckbox.classList.add(createdBook.id);
+    newCheckbox.type = "checkbox";
+    newCheckbox.checked = bookRead.checked;
+    newCheckbox.disabled = bookRead.checked;
+    newCheckbox.addEventListener("click", (event) => {
+      this.markRead(event.target, parseInt(event.target.classList[0]));
+    });
 
-    }
+    readData.appendChild(newCheckbox);
+    removeCell.appendChild(removeButton);
+    newTr.appendChild(titleData);
+    newTr.appendChild(authorData);
+    newTr.appendChild(readData);
+    newTr.appendChild(removeCell);
+    console.log(newTr);
 
-    markRead(checkbox, id) {
-        this.books.forEach((book) => {
-            if (id === book.id) {
-                book.read = true;
-                checkbox.disabled = true;
-            }
-        });
-    }    
+    table.appendChild(newTr);
+    console.log(this.book_count);
+  }
 
-    removeBook(bookId) {
-       this.books = this.books.filter(({id}) => bookId !== id);
+  markRead(checkbox, id) {
+    this.books.find((book) => {
+      if (id === book.id) {
+        book.read = true;
+        checkbox.disabled = true;
+      }
+    });
+  }
 
-       const tbody = document.getElementById("tableBody");
-       tbody.removeChild(document.getElementByClassName(bookId)[0]);
+  removeBook(id) {
+    this.books = this.books.filter((book) => {
+      return book.id != id;
+    });
+    console.log(this.books);
 
-        const removeButton = document.getElementById("remove");
-        removeButton.addEventListener("click", (event) => {
-            this.removeBook(parseInt(event.target.classList[0]))
-        });
-    }
+    const table = document.getElementById("tableBody");
+    const row = document.getElementById("bookRow" + id);
+    table.removeChild(row);
+  }
 }
 
 const library = new Library(books);
-
-const form = document.getElementById("form");
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    library.addBook();
+const bookAdd = document.getElementById("inputForm");
+bookAdd.addEventListener("submit", (event) => {
+  event.preventDefault();
+  library.addBook();
 });
 
-*/
+const bookOne = document.getElementById("bookRow1");
+const row1 = document.getElementById("firstRow");
+bookOne.addEventListener("click", (event) => {
+  event.preventDefault();
+  bookOne.remove(row1);
+});
